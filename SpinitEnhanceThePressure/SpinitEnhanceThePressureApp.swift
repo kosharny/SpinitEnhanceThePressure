26dd3d4d762db654c7daeadc60ee502fbef89dd8
Maksim Kosharny <maksimkosharnyjj@gmail.com>
2026-01-30 14:44:43 +0800

    Initial Commit

---
+++ SpinitEnhanceThePressure/SpinitEnhanceThePressureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinitEnhanceThePressureApp.swift
+//  SpinitEnhanceThePressure
+//
+//  Created by Maksim Kosharny on 30.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SpinitEnhanceThePressureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
